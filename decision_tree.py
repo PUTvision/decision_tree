e1a879342f295deddba3c1b10dd252abb9a2e9f0
Michal Fularz <michal.fularz@gmail.com>
2015-06-05 15:03:05 +0200

    Moved all the code related to decision trees to separate file.

--- decision_tree.py
+++ decision_tree.py
@@ -3,27 +3,6 @@ __author__ = 'Amin'
 import numpy as np
 import matplotlib.pyplot as plt
 
-# classification
-# training data
-training_data = [[0, 0], [1, 1]]
-class_labels = [0, 1]
-
-# decision tree
-# http://scikit-learn.org/stable/modules/tree.html
-from sklearn import tree
-
-tree_classifier = tree.DecisionTreeClassifier()
-tree_classifier = tree_classifier.fit(training_data, class_labels)
-print tree_classifier.predict([0.51, 0.51])
-
-# forest of randomized trees
-# http://scikit-learn.org/stable/modules/ensemble.html#forests-of-randomized-trees
-from sklearn.ensemble import RandomForestClassifier
-
-forest_classifier = RandomForestClassifier(n_estimators=10)
-forest_classifier = forest_classifier.fit(training_data, class_labels)
-print forest_classifier.predict([0.51, 0.51])
-
 from skimage import exposure
 from skimage import data
 from skimage.feature import local_binary_pattern
@@ -68,7 +47,8 @@ files_directory = "F:\\Amin\\Desktop\\INRIAPerson\\"
 
 # version 1
 # load each image in directory
-png_filenames = glob.glob(files_directory + "Train\\pos\\*.png")
+#png_filenames = glob.glob(files_directory + "Train\\pos\\*.png")
+png_filenames = glob.glob(files_directory + "96X160H96\\Train\\pos\\*.png")
 for filename in png_filenames[:5]:
     print filename
 
