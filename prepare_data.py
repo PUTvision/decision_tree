57e43ad1a4128e2c4a475bb4d37720d0f423ddb0
Michał Fularz <michal.fularz@gmail.com>
2015-06-09 15:19:52 +0200

    A lot of fixes - now we are using the right lbp variant (nri_uniform). A lot of refactoring to make the code maintainable.

--- prepare_data.py
+++ prepare_data.py
@@ -23,6 +23,7 @@ from sklearn.preprocessing import normalize
 
 import pickle
 
+
 # this is test function for checking different ways of calculating the histogram
 def calculate_histogram(image, number_of_bins):
     # different ways to calculate histogram
@@ -40,60 +41,18 @@ def calculate_histogram(image, number_of_bins):
     #normalized_histogram = normalize(img_as_float(histogram_from_np[:, np.newaxis]), axis=0).ravel()
     # this can also be done as a parameter for matplotlib hist method
 
-# settings for LBP
-radius = 3
-n_points = 8 * radius
-METHOD = 'uniform'
 
-# settings for histograms
-number_of_image_bins = 255
-number_of_lbp_bins = 25
-
-fig, (ax_img, ax_hist) = plt.subplots(nrows=2, ncols=2, figsize=(9, 6))
-plt.gray()
-plt.ion()
-plt.show()
-
-train_histogram_positive = []
-train_histogram_negative = []
-
-files_directory = "F:\\Amin\\Desktop\\INRIAPerson\\"
-
-# version 1
-# load each image in directory
-#png_filenames = glob.glob(files_directory + "Train\\pos\\*.png")
-png_filenames = glob.glob(files_directory + "96X160H96\\Train\\pos\\*.png")
-for filename in png_filenames[:10]:
-    print filename
-
-# version 2
-# load each image listed in specified text file
-#f = open(files_directory + "Train\\pos.lst", "r")
-#lines_from_file = f.read().split("\n")
-#image_from_file = None
-#for line in lines_from_file[:5]:
-    #print files_directory + line.replace("/", "\\") + "\n"
-    #filename = files_directory + line.replace("/", "\\")
-
-    image_from_file = img_as_ubyte(data.imread(filename, as_grey=True))
-    lbp_image = local_binary_pattern(image_from_file, n_points, radius, METHOD)
-
-    #calculate_histogram(lbp_image, number_of_lbp_bins)
-
+def show_histograms(original_image, lbp_image, nr_of_image_bins, nr_of_lbp_bins):
     # showing the results in the window
-    image_set = (image_from_file, lbp_image)
-    bins_set = (number_of_image_bins, number_of_lbp_bins)
-
-    lbp_histogram, lbp_bins, lbp_patches = plt.hist(lbp_image.ravel(), bins=number_of_lbp_bins, normed=False)
-
-    train_histogram_positive.append(lbp_histogram)
+    image_set = (original_image, lbp_image)
+    bins_set = (nr_of_image_bins, nr_of_lbp_bins)
 
     for ax, image in zip(ax_img, image_set):
         ax.imshow(image)
 
     for ax, image, number_of_bins in zip(ax_hist, image_set, bins_set):
         ax.cla()
-        ax.hist(image.ravel(), normed=False, bins=number_of_bins, range=(0, number_of_bins))
+        ax.hist(image.ravel(), normed=False, bins=number_of_bins)
 
     plt.draw()
 
@@ -103,50 +62,128 @@ for filename in png_filenames[:10]:
     #time.sleep(0.05)
     #raw_input("Press Enter to continue...")
 
-# prepare negative examples
-# version 1
-# load each image in directory
-#png_filenames = glob.glob(files_directory + "Train\\pos\\*.png")
-png_filenames = glob.glob(files_directory + "\\Train\\neg\\*.png")
-for filename in png_filenames[:10]:
+
+def get_histogram_of_file(filename, flag_use_part_of_image=False):
+    print filename
+
     image_from_file = img_as_ubyte(data.imread(filename, as_grey=True))
-    # we have to extract the patch of size 96x160 so negative samples will be the same as positive
-    width = 96
-    height = 160
-    # choose left upper corner
-    nrows, ncols = image_from_file.shape
-    #print nrows, ncols
-    x = random.randint(0, nrows-1-width)
-    y = random.randint(0, ncols-1-height)
-    #print x, y
-    part_of_image = image_from_file[x:x+width, y:y+height]
-    #print part_of_image
-    #viewer = ImageViewer(part_of_image)
-    #viewer.show()
+
+    if flag_use_part_of_image:
+        # we have to extract the patch of size 96x160 so negative samples will be the same as positive
+        width = 96
+        height = 160
+        # choose left upper corner
+        nrows, ncols = image_from_file.shape
+        #print nrows, ncols
+        x = random.randint(0, nrows-1-height)
+        y = random.randint(0, ncols-1-width)
+        #print x, y
+        part_of_image = image_from_file[x:x+height, y:y+width]
+        #print part_of_image
+        #viewer = ImageViewer(part_of_image)
+        #viewer.show()
+        image_from_file = part_of_image
+
     lbp_image = local_binary_pattern(image_from_file, n_points, radius, METHOD)
+
+    # example function for calculating histograms in different ways
+    #calculate_histogram(lbp_image, number_of_lbp_bins)
 
     lbp_histogram, lbp_bins, lbp_patches = plt.hist(lbp_image.ravel(), bins=number_of_lbp_bins, normed=False)
 
-    train_histogram_negative.append(lbp_histogram)
+    # function for showing the original and lbp images and theirs histograms
+    show_histograms(image_from_file, lbp_image, number_of_image_bins, number_of_lbp_bins)
+
+    print len(lbp_histogram)
+    print lbp_histogram
+
+    return lbp_histogram
+
+if __name__ == "__main__":
+    # settings for LBP
+    radius = 1
+    n_points = 8 * radius
+    METHOD = "nri_uniform"#'uniform'#"default"
+
+    # settings for histograms
+    number_of_image_bins = 256
+    number_of_lbp_bins = 59
+
+    fig, (ax_img, ax_hist) = plt.subplots(nrows=2, ncols=2, figsize=(9, 6))
+    plt.gray()
+    plt.ion()
+    plt.show()
+
+    number_of_positive_samples = 5
+    number_of_positive_tests = 1
+
+    number_of_negative_samples = 5
+    number_of_negative_tests = 1
+
+    train_histogram_positive = []
+    test_histogram_positive = []
+    train_histogram_negative = []
+    test_histogram_negative = []
+
+    files_directory = "F:\\Amin\\Desktop\\INRIAPerson\\"
+
+    # version 1
+    # load each image in directory
+    #png_filenames = glob.glob(files_directory + "Train\\pos\\*.png")
+    png_filenames = glob.glob(files_directory + "96X160H96\\Train\\pos\\*.png")
+    for filename in png_filenames[:number_of_positive_samples]:
+
+    # version 2
+    # load each image listed in specified text file
+    #f = open(files_directory + "Train\\pos.lst", "r")
+    #lines_from_file = f.read().split("\n")
+    #for line in lines_from_file[:5]:
+        #filename = files_directory + line.replace("/", "\\")
+
+        hist = get_histogram_of_file(filename)
+        train_histogram_positive.append(hist)
+
+    for filename in png_filenames[number_of_positive_samples:(number_of_positive_samples+number_of_positive_tests)]:
+
+        hist = get_histogram_of_file(filename)
+        test_histogram_positive.append(hist)
 
 
-print len(train_histogram_positive)
-with open("positive_histograms", "wb") as f:
-    pickle.dump(train_histogram_positive, f)
+    # prepare negative examples
+    png_filenames = glob.glob(files_directory + "\\Train\\neg\\*.png")
+    for filename in png_filenames[:number_of_negative_samples]:
+
+        hist = get_histogram_of_file(filename, flag_use_part_of_image=True)
+        train_histogram_negative.append(hist)
+
+    for filename in png_filenames[number_of_negative_samples:(number_of_negative_samples+number_of_negative_tests)]:
+
+        hist = get_histogram_of_file(filename, flag_use_part_of_image=True)
+        test_histogram_negative.append(hist)
 
 
-print len(train_histogram_negative)
-with open("negative_histograms", "wb") as f:
-    pickle.dump(train_histogram_negative, f)
+    print len(train_histogram_positive)
+    with open("positive_histograms", "wb") as f:
+        pickle.dump(train_histogram_positive, f)
+
+    print len(test_histogram_positive)
+    with open("test_positive_histograms", "wb") as f:
+        pickle.dump(test_histogram_positive, f)
+
+    print len(train_histogram_negative)
+    with open("negative_histograms", "wb") as f:
+        pickle.dump(train_histogram_negative, f)
+
+    print len(test_histogram_negative)
+    with open("test_negative_histograms", "wb") as f:
+        pickle.dump(test_histogram_negative, f)
 
 
-#with open("positive_histograms", "rb") as f:
-#    train_histogram_positive = pickle.load(f)
 
-#original_image = img_as_ubyte(data.imread("crop_000001a.png", as_grey=True))
-#original_image = img_as_ubyte(data.imread("F:\\Amin\\Desktop\\INRIAPerson\\Train\\pos\\person_and_bike_209.png", as_grey=True))
-#original_image = img_as_ubyte(rgb2gray(data.camera()))
-#original_image = img_as_ubyte(rgb2gray(data.lena()))
+    #original_image = img_as_ubyte(data.imread("crop_000001a.png", as_grey=True))
+    #original_image = img_as_ubyte(data.imread("F:\\Amin\\Desktop\\INRIAPerson\\Train\\pos\\person_and_bike_209.png", as_grey=True))
+    #original_image = img_as_ubyte(rgb2gray(data.camera()))
+    #original_image = img_as_ubyte(rgb2gray(data.lena()))
 
-#plt.bar(bins_from_np[:-1], normalized_histogram, width=1)
-#plt.xlim(min(bins_from_np), max(bins_from_np))
+    #plt.bar(bins_from_np[:-1], normalized_histogram, width=1)
+    #plt.xlim(min(bins_from_np), max(bins_from_np))
