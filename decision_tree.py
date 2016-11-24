edae6e64287d34c1cf86679d54e3cc45614b6db1
Michał Fularz <michal.fularz@gmail.com>
2016-11-24 17:41:31 +0100

    Added the module for data preparation for HoG and modified classification module to work with new input data and use linear SVM classifier. WARNING: there is some problem with scikit-learn 0.18.1 and it had to be downgraded to 0.18 see: http://stackoverflow.com/questions/40770170/scikit-learn-import-error

--- decision_tree.py
+++ decision_tree.py
@@ -11,9 +11,9 @@ from Tree import RandomForest
 
 
 def visualize_tree(clf, filename):
-    print "Number of splits: ", len(clf.tree_.value) #array of nodes values
-    print "Number of features: ", len(clf.tree.tree_.feature)
-    print "Number of thresholds: ", len(clf.tree.tree_.threshold)
+    print("Number of splits: ", len(clf.tree_.value)) #array of nodes values
+    print("Number of features: ", len(clf.tree.tree_.feature))
+    print("Number of thresholds: ", len(clf.tree.tree_.threshold))
 
     # save as *.dot file
     # with open(filename + ".dot", 'w') as f:
@@ -31,7 +31,7 @@ def visualize_tree(clf, filename):
 
 
 def visualize_forest(forest, filename_pattern):
-    print "Number of trees in the forest: ", forest.n_estimators
+    print("Number of trees in the forest: ", forest.n_estimators)
     counter = 0
     for clf in forest.estimators_[:]:
         filename = filename_pattern + str(counter)
@@ -53,17 +53,16 @@ def check_and_print_classifier_accuracy(clf, test_data_positive, test_data_negat
     correct_classifications += correct_n
     incorrect_classifications += incorrect_n
 
-    print "Accuracy: " + \
-          " overall: " + \
-          '% 2.4f' %(correct_classifications / (correct_classifications + incorrect_classifications)) + \
-          ", " + \
-          " positive: " + \
-          '% 2.4f' %(correct_p / (correct_p + incorrect_p)) + \
-          ", " + \
-          " negative: " + \
-          '% 2.4f' %(correct_n / (correct_n + incorrect_n)) + \
-          ""
-    pass
+    print("Accuracy: " +
+          " overall: " +
+          '% 2.4f' % (correct_classifications / (correct_classifications + incorrect_classifications)) + \
+          ", " +
+          " positive: " +
+          '% 2.4f' % (correct_p / (correct_p + incorrect_p)) +
+          ", " +
+          " negative: " +
+          '% 2.4f' % (correct_n / (correct_n + incorrect_n)) +
+          "")
 
 
 def test_classifier(clf, test_data, predicted_result):
@@ -71,7 +70,8 @@ def test_classifier(clf, test_data, predicted_result):
     incorrect_classifications = 0.0
 
     for data in test_data:
-        if clf.predict(data) == predicted_result:
+        # [something] is required as otherwise it is treated as 1d array, which generates warnings in scikit learn
+        if clf.predict([data]) == predicted_result:
             correct_classifications += 1.0
         else:
             incorrect_classifications += 1.0
@@ -90,71 +90,25 @@ def test_classification_performance(clf, test_data):
     if number_of_data_to_test < len(test_data):
         start = time.clock()
 
-        for i in xrange(0, number_of_iterations):
+        for i in range(0, number_of_iterations):
             for data in test_data[:number_of_data_to_test]:
                 clf.predict(data)
 
         end = time.clock()
         elapsed_time = (end - start)
 
-        print "It takes " + str(elapsed_time / number_of_iterations) + "us to classify " + \
-              str(number_of_data_to_test) + " data."
+        print("It takes " + str(elapsed_time / number_of_iterations) + "us to classify " + \
+              str(number_of_data_to_test) + " data.")
     else:
-        print "There is not enough data provided to evaluate the performance. It is required to provide at least " + \
-              str(number_of_data_to_test) + "values."
+        print("There is not enough data provided to evaluate the performance. It is required to provide at least " + \
+              str(number_of_data_to_test) + "values.")
 
-if __name__ == "__main__":
 
-    # prepare the training data
-    with open("data\\positive_histograms", "rb") as f:
-        train_histogram_positive = pickle.load(f)
-
-    class_labels_positive = [1] * len(train_histogram_positive)
-
-    with open("data\\test_positive_histograms", "rb") as f:
-        test_histogram_positive = pickle.load(f)
-
-    with open("data\\negative_histograms", "rb") as f:
-        train_histogram_negative = pickle.load(f)
-
-    class_labels_negative = [0] * len(train_histogram_negative)
-
-    with open("data\\test_negative_histograms", "rb") as f:
-        test_histogram_negative = pickle.load(f)
-
-    training_data = train_histogram_positive + train_histogram_negative
-    class_labels = class_labels_positive + class_labels_negative
-
-    # train and test the tree
-    classifier = None
-
-    from sklearn.tree import DecisionTreeClassifier
-    from sklearn.ensemble import RandomForestClassifier
-
-    #for nr_of_trees in range(51, 52, 10):
-    for nr_of_trees in range(2, 3):
-        #for depth in range(1, 21):
-        for depth in range(21, 22):
-
-            print( "Parameters: depth: " + '% 02.0f' %depth + \
-                  ", nr of trees: " + '% 02.0f' %nr_of_trees + \
-                  "): ")
-
-            classifier = DecisionTreeClassifier(max_depth=depth)
-            classifier = classifier.fit(training_data, class_labels)
-
-            #classifier = RandomForestClassifier(n_estimators=nr_of_trees, max_depth=depth)
-            #classifier = classifier.fit(training_data, class_labels)
-
-            check_and_print_classifier_accuracy(classifier, test_histogram_positive, test_histogram_negative)
-
-    # Use this to test the performance
-    #test_classification_performance(classifier, test_histogram_positive)
-
+def generate_my_classifier(classifier):
     list_of_input_value_names = []
     # TODO - this value should be taken automatically
     #for i in xrange(0, 3540):
-    for i in xrange(0, 6000):
+    for i in range(0, 6000):
         list_of_input_value_names.append(i)
 
     if isinstance(classifier, DecisionTreeClassifier):
@@ -172,14 +126,14 @@ if __name__ == "__main__":
 
     my_classifier.print_parameters()
 
-    for histogram in test_histogram_positive:
+    for histogram in test_data_positive:
         scikit_learn_result = classifier.predict(histogram)
         my_result = my_classifier.predict(histogram)
 
         if scikit_learn_result != my_result:
             print("Error!")
 
-    for histogram in test_histogram_negative:
+    for histogram in test_data_negative:
         scikit_learn_result = classifier.predict(histogram)
         my_result = my_classifier.predict(histogram)
 
@@ -194,3 +148,62 @@ if __name__ == "__main__":
 
     #visualize_forest(classifier, "tree_visualization\\tree")
     #visualize_tree(classifier, "tree_visualization\\tree")
+
+if __name__ == "__main__":
+
+    # version for HOG
+    data_filename = "data"
+    # version for LBP
+    #data_filename = "histograms"
+
+    # prepare the training data
+    with open("data\\positive_" + str(data_filename), "rb") as f:
+        train_data_positive = pickle.load(f)
+
+    class_labels_positive = [1] * len(train_data_positive)
+
+    with open("data\\test_positive_" + str(data_filename), "rb") as f:
+        test_data_positive = pickle.load(f)
+
+    with open("data\\negative_" + str(data_filename), "rb") as f:
+        train_data_negative = pickle.load(f)
+
+    class_labels_negative = [0] * len(train_data_negative)
+
+    with open("data\\test_negative_" + str(data_filename), "rb") as f:
+        test_data_negative = pickle.load(f)
+
+    training_data = train_data_positive + train_data_negative
+    class_labels = class_labels_positive + class_labels_negative
+
+    # train and test the classifier
+    classifier = None
+
+    from sklearn.tree import DecisionTreeClassifier
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.svm import LinearSVC
+
+    #for nr_of_trees in range(51, 52, 10):
+    for nr_of_trees in range(2, 3):
+        #for depth in range(1, 21):
+        for depth in range(21, 22):
+
+            print("Parameters: depth: " + '% 02.0f' % depth +
+                  ", nr of trees: " + '% 02.0f' % nr_of_trees +
+                  "): ")
+
+            # classifier = DecisionTreeClassifier(max_depth=depth)
+            # classifier = classifier.fit(training_data, class_labels)
+
+            #classifier = RandomForestClassifier(n_estimators=nr_of_trees, max_depth=depth)
+            #classifier = classifier.fit(training_data, class_labels)
+
+            classfier = LinearSVC()
+            classifier = classfier.fit(training_data, class_labels)
+
+            check_and_print_classifier_accuracy(classifier, test_data_positive, test_data_negative)
+
+    # Use this to test the performance (speed of execution)
+    #test_classification_performance(classifier, test_data_positive)
+
+    #generate_my_classifier(classfier)
---
+++ prepare_data_hog.py
@@ -0,0 +1,125 @@
+import skimage.data
+import skimage.feature
+import skimage
+import matplotlib.pyplot as plt
+import glob
+import random
+import pickle
+import HOG_modified
+
+#####################################
+# SET THE FOLLOWING PARAMETERS
+# INRIA DATABASE FOR HOG (64x128)
+width = 64
+height = 128
+region_size = 16
+number_of_positive_samples = 200
+number_of_positive_tests = 100
+number_of_negative_samples = 200
+number_of_negative_tests = 100
+files_directory = "F:\\Amin\\Desktop\\INRIAPerson\\"
+positive_samples_directory = files_directory + "70X134H96\\Test\\pos\\"
+negative_train_samples_directory = files_directory + "\\Train\\neg\\"
+negative_test_samples_directory = files_directory + "\\Test\\neg\\"
+# END OF PARAMETERS SETTING
+#####################################
+
+def show_images(original, hog):
+    fig, (ax_image_original, ax_image_hog) = plt.subplots(1, 2, figsize=(8, 4), sharex=True, sharey=True)
+
+    ax_image_original.axis('off')
+    ax_image_original.imshow(original, cmap=plt.cm.gray)
+    ax_image_original.set_title('Input image')
+    ax_image_original.set_adjustable('box-forced')
+
+    # Rescale histogram for better display
+    hog_image_rescaled = skimage.exposure.rescale_intensity(hog, in_range=(0, 0.02))
+
+    ax_image_hog.axis('off')
+    ax_image_hog.imshow(hog_image_rescaled, cmap=plt.cm.gray)
+    ax_image_hog.set_title('Histogram of Oriented Gradients')
+    ax_image_hog.set_adjustable('box-forced')
+    plt.draw()
+
+    plt.pause(0.0001)
+    plt.waitforbuttonpress()
+
+    plt.close()
+
+
+def get_description_of_image_from_file(filename, flag_use_part_of_image=False, show=False):
+    print(filename)
+
+    image_from_file = skimage.data.imread(filename, as_grey=True)
+
+    nrows, ncols = image_from_file.shape
+
+    if flag_use_part_of_image:
+        # choose left upper corner
+        if nrows > height and ncols > width:
+            x = random.randint(0, nrows - 1 - height)
+            y = random.randint(0, ncols - 1 - width)
+            # print x, y
+            part_of_image = image_from_file[x:x + height, y:y + width]
+            # print part_of_image
+            image_from_file = part_of_image
+    else:
+        # crop the image to 64 x 128 pixel size
+        image_from_file = skimage.util.crop(image_from_file, 3)
+
+    # TODO - should we add global image normalisation?
+    # http://scikit-image.org/docs/dev/api/skimage.feature.html?highlight=hog#skimage.feature.hog
+    feature_vector, hog_image = skimage.feature.hog(image_from_file, orientations=9, pixels_per_cell=(8, 8),
+                    cells_per_block=(2, 2), visualise=True)
+
+    if len(feature_vector) != 3780:
+        print("Wrong feature vector size for specified HOG parameters. Should be: 3780, while it is: " + str(len(feature_vector)))
+
+    if show:
+        show_images(image_from_file, hog_image)
+
+    return feature_vector
+
+
+if __name__ == "__main__":
+    train_data_positive = []
+    test_data_positive = []
+    train_data_negative = []
+    test_data_negative = []
+
+    # prepare positive examples
+    png_filenames = glob.glob(positive_samples_directory + "*.png")
+    for filename in png_filenames[:number_of_positive_samples]:
+        data = get_description_of_image_from_file(filename, show=False)
+        train_data_positive.append(data)
+
+    for filename in png_filenames[number_of_positive_samples:(number_of_positive_samples+number_of_positive_tests)]:
+        data = get_description_of_image_from_file(filename, show=False)
+        test_data_positive.append(data)
+
+    # prepare negative examples
+    png_filenames = glob.glob(negative_train_samples_directory + "*.png") + glob.glob(negative_train_samples_directory + "*.jpg")
+    for filename in png_filenames[:number_of_negative_samples]:
+        data = get_description_of_image_from_file(filename, flag_use_part_of_image=True, show=False)
+        train_data_negative.append(data)
+
+    png_filenames = glob.glob(negative_test_samples_directory + "*.png") + glob.glob(negative_test_samples_directory + "*.jpg")
+    for filename in png_filenames[:number_of_negative_tests]:
+        data = get_description_of_image_from_file(filename, flag_use_part_of_image=True, show=False)
+        test_data_negative.append(data)
+
+    print(len(train_data_positive))
+    with open("data\\positive_data", "wb") as f:
+        pickle.dump(train_data_positive, f)
+
+    print(len(test_data_positive))
+    with open("data\\test_positive_data", "wb") as f:
+        pickle.dump(test_data_positive, f)
+
+    print(len(train_data_negative))
+    with open("data\\negative_data", "wb") as f:
+        pickle.dump(train_data_negative, f)
+
+    print(len(test_data_negative))
+    with open("data\\test_negative_data", "wb") as f:
+        pickle.dump(test_data_negative, f)
