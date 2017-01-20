6321076c3444499cb7defb5d62be8b8c9d97cfff
Michał Fularz <michal.fularz@gmail.com>
2017-01-20 19:33:56 +0100

    A lot of changes... some back and forth as I was thinking that that is the last one and then it all started again. In a nutshell additional semi-software implementation was added (with atan), that introduced some more bugs. When it was all sorted out it turned out that it is not working as good as hog from scikit-image.

--- HOG_modified.py
+++ HOG_modified.py
@@ -2,12 +2,98 @@ __author__ = 'Amin'
 
 import numpy as np
 import sklearn.preprocessing
+import math
+import matplotlib.pyplot as plt
 
 
 class GradientDir:
 
     def __init__(self):
         pass
+
+    def compare_implemenatation(self):
+        test_size = 50
+
+        pixel_descriptors_tangent = np.empty((test_size*2, test_size*2), dtype=object)
+        pixel_descriptors_vhdl = np.empty((test_size*2, test_size*2), dtype=object)
+
+        row_to_check = -30
+        col_to_check = -17
+
+        number_of_differences = 0
+
+        for row in range(-test_size, test_size+1):
+            for col in range(-test_size, test_size+1):
+                gradient_x = row
+                gradient_y = col
+
+                gradient_norm = self._gradient_norm(gradient_x, gradient_y)
+
+                if row == row_to_check and col == col_to_check:
+                    print("Debug go!")
+
+                pd = self._compute_and_combine_dir_and_demux_gradien(gradient_x, gradient_y, gradient_norm)
+                pixel_descriptors_tangent[row][col] = np.asarray(pd.ravel().tolist())
+
+                abs_gradient_x, abs_gradient_y, sign_flag = self._shuffle_signs(gradient_x, gradient_y)
+                bins_0, bins_1 = self._compute_dir(abs_gradient_x, abs_gradient_y, sign_flag)
+                gradient_dir = self._combine_dir(bins_0, bins_1)
+
+                pixel_descriptor = self._gradient_demux(gradient_dir, gradient_norm)
+                #pixel_descriptor = self._downto_fix(pixel_descriptor)
+                pixel_descriptor = self._bin_mixing(pixel_descriptor)
+
+                pd_diff = pd - pixel_descriptor
+                if not np.all(pd_diff == 0):
+                    number_of_differences += 1
+                    print(pd)
+                    print(pixel_descriptor)
+                    #print("row: " + str(row) + ", col: " + str(col))
+                    if gradient_x == 0 and gradient_y == 0:
+                        angle = 0
+                    # both the +inf and -inf are put to the same value, as either way it will go to the same bins
+                    elif gradient_x == 0 and gradient_y != 0:
+                        angle = -math.pi / 2  # also could be math.pi/2
+                    else:
+                        angle = math.atan(gradient_y / gradient_x)
+
+                    angle_in_degrees = angle * 180 / math.pi
+
+                    print("x: " + str(gradient_x) + ", y: " + str(gradient_y) + ", angle: " + str(angle_in_degrees))
+
+
+
+                pixel_descriptors_vhdl[row][col] = np.asarray(pixel_descriptor)
+
+        pixel_descriptors_difference = pixel_descriptors_tangent - pixel_descriptors_vhdl
+
+        print("Number pf differences: " + str(number_of_differences))
+        print("Done!")
+
+    def _downto_fix(self, pixel_descriptor):
+        import copy
+        temp = copy.deepcopy(pixel_descriptor)
+        for i in range(0, len(pixel_descriptor)):
+            pixel_descriptor[i] = temp[len(pixel_descriptor)-1 - i]
+
+        return pixel_descriptor
+
+    def _bin_mixing(self, pixel_descriptor):
+        import copy
+        temp = copy.deepcopy(pixel_descriptor)
+        pixel_descriptor[0] = temp[4]
+        pixel_descriptor[1] = temp[5]
+        pixel_descriptor[2] = temp[6]
+        pixel_descriptor[3] = temp[7]
+        pixel_descriptor[4] = temp[8]
+        pixel_descriptor[5] = temp[0]
+        pixel_descriptor[6] = temp[1]
+        pixel_descriptor[7] = temp[2]
+        pixel_descriptor[8] = temp[3]
+
+        return pixel_descriptor
+
+        return pixel_descriptor
 
     def compute(self, image: np.ndarray):
 
@@ -44,9 +130,14 @@ class GradientDir:
                     #print("gradient_norm: (" + str(row) + ", " + str(col) + "): " + str(gradient_norm))
 
                 pixel_descriptor = self._gradient_demux(gradient_dir, gradient_norm)
+                pixel_descriptor = self._bin_mixing(pixel_descriptor)
                 #print("pixel_descriptor: " + str(pixel_descriptor))
 
+                ##pd = self._compute_and_combine_dir_and_demux_gradien(gradient_x, gradient_y, gradient_norm)
+                ##pixel_descriptor = pd.ravel().tolist()
+
                 pixel_descriptors[row][col] = np.asarray(pixel_descriptor)
+
 
         #print("pixel_descriptors: \n" + str(pixel_descriptors))
 
@@ -72,6 +163,103 @@ class GradientDir:
 
         return abs_gradient_x, abs_gradient_y, sign_flag
 
+    # TODO - zamień na arctan(y/x) - 0 - 180 stopni
+    # TODO - i potem mając kąt umieścić wagę go do dwóch sąsiednich "binów"
+    # TODO - liniowo podzielić
+    # TODO - na koniec nie zapomnieć, żeby przemnożyć uzyskane wagi przez grad_norm
+
+    def _compute_and_combine_dir_and_demux_gradien(self, gradient_x, gradient_y, gradient_norm):
+
+        # TODO - think how to handle division by 0 properly
+        if gradient_x == 0 and gradient_y == 0:
+            angle = 0
+        # both the +inf and -inf are put to the same value, as either way it will go to the same bins
+        elif gradient_x == 0 and gradient_y != 0:
+            angle = -math.pi/2  # also could be math.pi/2
+        else:
+            angle = math.atan(gradient_y/gradient_x)
+
+        angle_in_degrees = angle*180/math.pi
+
+        #if angle_in_degrees != 0:
+            #print("x: " + str(gradient_x) + ", y: " + str(gradient_y) + ", angle: " + str(angle_in_degrees))
+
+        # place the angle into appropriate bins
+        bins_center_angle = []
+        for i in range(-80, 100, 20):
+            bins_center_angle.append(float(i))
+        #print(bins_center_angle)
+
+        bins_values = np.zeros([1, 9])
+        #print(bins_values)
+
+        # if angle_in_degrees <= -80:
+        #     bins_values[0, 0] = 0.5
+        #     bins_values[0, 1] = 0.5
+        # elif angle_in_degrees <= -70:
+        #     bins_values[0, 0] = 0.5
+        #     bins_values[0, 1] = 0.5
+        # elif angle_in_degrees <= -50:
+        #     bins_values[0, 1] = 0.5
+        #     bins_values[0, 2] = 0.5
+        # elif angle_in_degrees <= -30:
+        #     bins_values[0, 2] = 0.5
+        #     bins_values[0, 3] = 0.5
+        # elif angle_in_degrees <= -10:
+        #     bins_values[0, 3] = 0.5
+        #     bins_values[0, 4] = 0.5
+        # elif angle_in_degrees <= 10:
+        #     bins_values[0, 4] = 0.5
+        #     bins_values[0, 5] = 0.5
+        # elif angle_in_degrees <= 30:
+        #     bins_values[0, 5] = 0.5
+        #     bins_values[0, 6] = 0.5
+        # elif angle_in_degrees <= 50:
+        #     bins_values[0, 6] = 0.5
+        #     bins_values[0, 7] = 0.5
+        # elif angle_in_degrees <= 70:
+        #     bins_values[0, 7] = 0.5
+        #     bins_values[0, 8] = 0.5
+        # else:
+        #     bins_values[0, 8] = 0.5
+        #     bins_values[0, 0] = 0.5
+
+        # TODO - take care of first and last range (should they be connected?)
+        # check if the angle is smaller than center point of first bin
+        if angle_in_degrees < bins_center_angle[0]:
+            distance_to_current = abs(angle_in_degrees - bins_center_angle[0])
+            distance_to_next = 20 - distance_to_current
+            bins_values[0, 0] = 0.5#distance_to_next/20
+            bins_values[0, 1] = 0.5#distance_to_current/20
+        elif angle_in_degrees > bins_center_angle[-1]:
+            distance_to_current = abs(angle_in_degrees - bins_center_angle[-1])
+            distance_to_next = 20 - distance_to_current
+            bins_values[0, 0] = 0.5#distance_to_next/20
+            bins_values[0, 8] = 0.5#distance_to_current/20
+        else:
+            for i, [current_center_angle, next_center_angle] in enumerate(zip(bins_center_angle[:-1], bins_center_angle[1:])):
+                if angle_in_degrees == current_center_angle:
+                    #print("Found exactly: " + str(current_center_angle))
+                    #bins_values[0, i] = 1
+                    bins_values[0, i] = 0.5
+                    bins_values[0, i+1] = 0.5
+
+                elif current_center_angle < angle_in_degrees < next_center_angle:
+                    #print("Found: " + str(i) + ", current_center_angle: " + str(current_center_angle) + ", next_center_angle: " + str(next_center_angle))
+
+                    distance_to_current = abs(angle_in_degrees - current_center_angle)
+                    distance_to_next = abs(angle_in_degrees - next_center_angle)
+
+                    bins_values[0, i] = 0.5#distance_to_next/20
+                    bins_values[0, i+1] = 0.5#distance_to_current/20
+
+        # multiply by grad_norm
+        bins_values = bins_values * gradient_norm
+        #if bins_values.any():   # check if all are not 0
+            #print(bins_values)
+
+        return bins_values
+
     def _compute_dir(self, abs_gradient_x, abs_gradient_y, sign_flag):
         # watch out for approximations of values - here we divide, while in VHDL some bits are removed, so:
         # I guess it is ok, cause // operator was used (no reminder)
@@ -84,56 +272,63 @@ class GradientDir:
         mul_coeff_1_2 = (abs_gradient_x * 1220) // 1024
         mul_coeff_1_3 = (abs_gradient_x * 2813) // 1024
 
-        # TODO - in VHDL it is possible to remove the comparison with 0
-        if abs_gradient_y < mul_coeff_0_0:
-            if not sign_flag:
-                bins_0 = 0b000000001
-            else:
-                bins_0 = 0b100000000
-        elif mul_coeff_0_0 <= abs_gradient_y < mul_coeff_0_1:
-            if not sign_flag:
-                bins_0 = 0b000000010
-            else:
-                bins_0 = 0b010000000
-        elif mul_coeff_0_1 <= abs_gradient_y < mul_coeff_0_2:
-            if not sign_flag:
-                bins_0 = 0b000000100
-            else:
-                bins_0 = 0b001000000
-        elif mul_coeff_0_2 <= abs_gradient_y < mul_coeff_0_3:
-            if not sign_flag:
-                bins_0 = 0b000001000
-            else:
-                bins_0 = 0b000100000
-        else:  # abs_gradient_y >= mul_coeff_0_3
-            bins_0 = 0b000010000
-
-        if abs_gradient_y < mul_coeff_1_0:
+        if abs_gradient_x == 0 and abs_gradient_y == 0:
+            bins_0 = 0b000000001
             bins_1 = 0b100000000
-        elif mul_coeff_1_0 <= abs_gradient_y < mul_coeff_1_1:
-            if not sign_flag:
-                bins_1 = 0b000000001
-            else:
-                bins_1 = 0b010000000
-        elif mul_coeff_1_1 <= abs_gradient_y < mul_coeff_1_2:
-            if not sign_flag:
-                bins_1 = 0b000000010
-            else:
-                bins_1 = 0b001000000
-        elif mul_coeff_1_2 <= abs_gradient_y < mul_coeff_1_3:
-            if not sign_flag:
-                bins_1 = 0b000000100
-            else:
-                bins_1 = 0b000100000
-        else:  # abs_gradient_y >= mul_coeff_1_3
-            if not sign_flag:
-                bins_1 = 0b000001000
-            else:
-                bins_1 = 0b000010000
+        elif abs_gradient_x == 0 and abs_gradient_y != 0:
+            bins_0 = 0b000010000
+            bins_1 = 0b000010000
+        else:
+            # TODO - in VHDL it is possible to remove the comparison with 0
+            if abs_gradient_y < mul_coeff_0_0:
+                if not sign_flag:
+                    bins_0 = 0b000000001
+                else:
+                    bins_0 = 0b100000000
+            elif mul_coeff_0_0 <= abs_gradient_y < mul_coeff_0_1:
+                if not sign_flag:
+                    bins_0 = 0b000000010
+                else:
+                    bins_0 = 0b010000000
+            elif mul_coeff_0_1 <= abs_gradient_y < mul_coeff_0_2:
+                if not sign_flag:
+                    bins_0 = 0b000000100
+                else:
+                    bins_0 = 0b001000000
+            elif mul_coeff_0_2 <= abs_gradient_y < mul_coeff_0_3:
+                if not sign_flag:
+                    bins_0 = 0b000001000
+                else:
+                    bins_0 = 0b000100000
+            else:  # abs_gradient_y >= mul_coeff_0_3
+                bins_0 = 0b000010000
+
+            if abs_gradient_y < mul_coeff_1_0:
+                bins_1 = 0b100000000
+            elif mul_coeff_1_0 <= abs_gradient_y < mul_coeff_1_1:
+                if not sign_flag:
+                    bins_1 = 0b000000001
+                else:
+                    bins_1 = 0b010000000
+            elif mul_coeff_1_1 <= abs_gradient_y < mul_coeff_1_2:
+                if not sign_flag:
+                    bins_1 = 0b000000010
+                else:
+                    bins_1 = 0b001000000
+            elif mul_coeff_1_2 <= abs_gradient_y < mul_coeff_1_3:
+                if not sign_flag:
+                    bins_1 = 0b000000100
+                else:
+                    bins_1 = 0b000100000
+            else:  # abs_gradient_y >= mul_coeff_1_3
+                if not sign_flag:
+                    bins_1 = 0b000001000
+                else:
+                    bins_1 = 0b000010000
 
         return bins_0, bins_1
 
-    def _combine_dir(self, bins_0, bins_1):
+    def _combine_dir_old(self, bins_0, bins_1):
         bin_number = 0b000000000000000000  # bit length: 18 (elements are indexed 0 through 17)
         for i in range(8, -1, -1):
             bins_0_bit = bins_0 & (1 << i)
@@ -142,6 +337,26 @@ class GradientDir:
             v2 = ((bins_0_bit ^ bins_1_bit) >> i)
             bin_number |= (v1 << (2*i+1))
             bin_number |= (v2 << (2*i))
+
+        return bin_number
+
+    def _combine_dir(self, bins_0, bins_1):
+        # zero the most significant bit
+        additional_bins = 0b011111111 & bins_1
+        #print(bin(additional_bins))
+        # move it to the left by one
+        additional_bins = additional_bins << 1
+        #print(bin(additional_bins))
+        # insert the last bit from bins_0 to the first position
+        temp_bins = 0b100000000 & bins_1
+        #print(bin(temp_bins))
+        temp_bins = temp_bins >> 8
+        #print(bin(temp_bins))
+
+        additional_bins = additional_bins | temp_bins
+        #print(bin(additional_bins))
+
+        bin_number = bins_0 | bins_1 | additional_bins
 
         return bin_number
 
@@ -160,7 +375,10 @@ class GradientDir:
         else:
             return maximum
 
-    def _gradient_demux(self, gradient_dir, gradient_norm):
+    def _gradient_norm2(self, grad_x, grad_y):
+        return math.sqrt(grad_x ** 2 + grad_y ** 2)
+
+    def _gradient_demux_old(self, gradient_dir, gradient_norm):
         out = [0]*9
 
         if gradient_dir == 0b000000000000000101:
@@ -208,6 +426,43 @@ class GradientDir:
             out[7] = gradient_norm * 2
         elif gradient_dir == 0b100000000000000000:
             out[8] = gradient_norm * 2
+
+        return out
+
+    def _gradient_demux(self, gradient_dir, gradient_norm):
+        out = [0]*9
+
+        if gradient_dir == 0b000000011:
+            out[0] = gradient_norm/2
+            out[1] = gradient_norm/2
+        elif gradient_dir == 0b000000110:
+            out[1] = gradient_norm/2
+            out[2] = gradient_norm/2
+        elif gradient_dir == 0b000001100:
+            out[2] = gradient_norm/2
+            out[3] = gradient_norm/2
+        elif gradient_dir == 0b000011000:
+            out[3] = gradient_norm/2
+            out[4] = gradient_norm/2
+        elif gradient_dir == 0b000110000:
+            out[4] = gradient_norm/2
+            out[5] = gradient_norm/2
+        elif gradient_dir == 0b001100000:
+            out[5] = gradient_norm/2
+            out[6] = gradient_norm/2
+        elif gradient_dir == 0b011000000:
+            out[6] = gradient_norm/2
+            out[7] = gradient_norm/2
+        elif gradient_dir == 0b110000000:
+            out[7] = gradient_norm/2
+            out[8] = gradient_norm/2
+        elif gradient_dir == 0b100000001:
+            out[8] = gradient_norm/2
+            out[0] = gradient_norm/2
+        else:
+            print("error!")
+            out[0] = gradient_norm/2
+            out[1] = gradient_norm/2
 
         return out
 
@@ -286,8 +541,12 @@ def hog(image, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2)):
 
 if __name__ == "__main__":
 
-    gradients = GradientDir()
+    #gradients = GradientDir()
+    #gradients.compare_implemenatation()
 
+
+
+    # gradients = GradientDir()
     # abs_gradient_x, abs_gradient_y, sign_flag = gradients._shuffle_signs(-255, -255)
     # bins_0, bins_1 = gradients._compute_dir(abs_gradient_x, abs_gradient_y, sign_flag)
     # gradient_dir = gradients._combine_dir(bins_0, bins_1)
