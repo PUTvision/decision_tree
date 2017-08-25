fad32744da83ddb08f9f3bba19b9feaf3145692e
Michał Fularz <michal.fularz@gmail.com>
2017-08-25 16:15:43 +0200

    Added test for fixed point calculations, improved dataset_tester.

--- dataset_tester.py
+++ dataset_tester.py
@@ -2,14 +2,26 @@ import numpy as np
 
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
-
 from sklearn import metrics
+
+import time
 
 
 def report_classifier(clf, expected, predicted):
     print("Classification report for classifier %s:\n%s\n"
           % (clf, metrics.classification_report(expected, predicted)))
     print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
+
+    correct_classifications = 0
+    incorrect_classifications = 0
+    for e, p in zip(expected, predicted):
+        if e == p:
+            correct_classifications += 1
+        else:
+            incorrect_classifications += 1
+    print("Accuracy overall: " +
+          '% 2.4f' % (correct_classifications / (correct_classifications + incorrect_classifications))
+          )
 
 
 def compare_with_own_classifier(scikit_clf, own_clf, test_data):
@@ -32,14 +44,17 @@ def compare_with_own_classifier(scikit_clf, own_clf, test_data):
         print("Number of errors: " + str(number_of_errors))
 
 
-def test_dataset(number_of_classes: int, train_data: np.ndarray, train_target: np.ndarray, test_data: np.ndarray, test_target: np.ndarray):
+def test_dataset(number_of_features: int, number_of_bits_per_feature: int,
+                 train_data: np.ndarray, train_target: np.ndarray,
+                 test_data: np.ndarray, test_target: np.ndarray
+                 ):
     clf_decision_tree = DecisionTreeClassifier()  # max_depth=50)
     clf_decision_tree.fit(train_data, train_target)
     test_predicted = clf_decision_tree.predict(test_data)
     report_classifier(clf_decision_tree, test_target, test_predicted)
 
     from tree import Tree
-    my_clf_decision_tree = Tree("TreeTest", number_of_classes)
+    my_clf_decision_tree = Tree("TreeTest", number_of_features, number_of_bits_per_feature)
     my_clf_decision_tree.build(clf_decision_tree)
     my_clf_decision_tree.print_parameters()
     my_clf_decision_tree.create_vhdl_file()
@@ -52,9 +67,29 @@ def test_dataset(number_of_classes: int, train_data: np.ndarray, train_target: n
     report_classifier(clf_decision_tree, test_target, test_predicted)
 
     from tree import RandomForest
-    my_clf_random_forest = RandomForest(number_of_classes)
+    my_clf_random_forest = RandomForest(number_of_features, number_of_bits_per_feature)
     my_clf_random_forest.build(clf_random_forest)
     my_clf_random_forest.print_parameters()
     my_clf_random_forest.create_vhdl_file()
 
     compare_with_own_classifier(clf_random_forest, my_clf_random_forest, test_data)
+
+
+def test_classification_performance(clf, test_data, number_of_data_to_test=1000, number_of_iterations=1000):
+    if number_of_data_to_test <= len(test_data):
+        start = time.clock()
+
+        for i in range(0, number_of_iterations):
+            for data in test_data[:number_of_data_to_test]:
+                clf.predict([data])
+
+        end = time.clock()
+        elapsed_time = (end - start)
+
+        print("It takes " +
+              '% 2.4f' % (elapsed_time / number_of_iterations) +
+              "us to classify " +
+              str(number_of_data_to_test) + " data.")
+    else:
+        print("There is not enough data provided to evaluate the performance. It is required to provide at least " +
+              str(number_of_data_to_test) + " values.")
--- tests/fixed_point_tests.py
+++ tests/fixed_point_tests.py
@@ -7,8 +7,10 @@ def convert_to_fixed_point(float_value, n_bits):
     return np.round(float_value * f) * (1.0 / f)
 
 if __name__ == "__main__":
-    value = 0.25984365873645
+    value = 13/16#1/3
     print(value)
 
-    for i in range(10, 1, -1):
-        print("Number of bits used: " + str(i) + " value: " + str(convert_to_fixed_point(value, i)))
+    for i in range(10, 0, -1):
+        value_as_fixed_point = convert_to_fixed_point(value, i)
+        print("Number of bits used: " + str(i) + " value: " + str(value_as_fixed_point))
+        print("% error: {0:.2f}".format(abs(value - value_as_fixed_point)/value*100))
