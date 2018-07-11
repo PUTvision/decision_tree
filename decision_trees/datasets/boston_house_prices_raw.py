87f00793277fbbd5aab8a8efdc601333d0cab57e
Michał Fularz <michal.fularz@gmail.com>
2018-07-11 14:51:22 +0200

    Added boston database and sample code for it.

---
+++ decision_trees/datasets/boston_house_prices_raw.py
@@ -0,0 +1,86 @@
+import numpy as np
+from sklearn import datasets
+from sklearn.utils import shuffle
+
+from decision_trees.datasets.dataset_base import DatasetBase
+
+
+class BostonRaw(DatasetBase):
+    def __init__(self, number_of_train_samples: int, number_of_test_samples: int):
+        self._number_of_train_samples = number_of_train_samples
+        self._number_of_test_samples = number_of_test_samples
+
+    def _load_data(self):
+        boston = datasets.load_boston()
+        # print(boston.data.shape)
+        # print(boston.target.shape)
+        # print(np.unique(boston.target))
+
+        # it is necessary to shuffle the data as all 0's are at the front and all 9's are at the end
+        boston.data, boston.target = shuffle(boston.data, boston.target)
+
+        train_data = boston.data[:self._number_of_train_samples]
+        train_target = boston.target[:self._number_of_train_samples]
+        test_data = boston.data[self._number_of_train_samples:self._number_of_train_samples+self._number_of_test_samples]
+        test_target = boston.target[self._number_of_train_samples:self._number_of_train_samples+self._number_of_test_samples]
+
+        return train_data, train_target, test_data, test_target
+
+    @staticmethod
+    def _normalise(data: np.ndarray):
+        # in case of MNIST data it is possible to just divide each data by maximum value
+        # each feature is in range 0-255
+        data = data / 255
+
+        return data
+
+
+def test_boston_raw():
+    #####################################
+    # SET THE FOLLOWING PARAMETERS
+    # Boston House Prices DATABASE
+    # total number of samples: 506 (each is 13 values)
+    number_of_train_samples = 400
+    number_of_test_samples = 100
+    # END OF PARAMETERS SETTING
+    if (number_of_train_samples + number_of_test_samples) > 506:
+        print("ERROR, too much samples set!")
+    #####################################
+
+    d = BostonRaw(number_of_train_samples, number_of_test_samples)
+    d.run()
+
+    assert True
+
+
+if __name__ == "__main__":
+    d = BostonRaw(400, 100)
+
+    train_data, train_target, test_data, test_target = d._load_data()
+
+    print(f"train_data.shape: {train_data.shape}")
+
+    # train_data = d._normalise(train_data)
+    # test_data = d._normalise(test_data)
+
+    from decision_trees import dataset_tester
+
+    # dataset_tester.perform_experiment(train_data[:60000], train_target[:60000],
+    #                                   test_data[:10000], test_target[:10000],
+    #                                   8-1
+    #                                   )
+
+    from sklearn.ensemble import RandomForestRegressor
+
+    clf = RandomForestRegressor(n_estimators=10, max_depth=None, n_jobs=3, random_state=42)
+    clf.fit(train_data, train_target)
+    test_predicted = clf.predict(test_data)
+
+    diffs = test_target - test_predicted
+    for diff, expected, predicted in zip(diffs, test_target, test_predicted):
+        print(f"expected: {expected}, predicted: {predicted}, diff: {diff:{1}.{3}}, relative error: {diff/expected:{1}.{3}}")
+
+    # dataset_tester.test_dataset(40,
+    #                             train_data, train_target, test_data, test_target,
+    #                             dataset_tester.ClassifierType.random_forest_regressor,
+    #                             )
